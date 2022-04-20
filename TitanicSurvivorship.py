7b032fa8c9b43ded712a3ae6f52ae04024bf7ddd
PhilosophyWithJosh <joshmendiola@icloud.com>
2022-04-20 15:55:15 -0500

    exploring datasets

--- TitanicSurvivorship.py
+++ TitanicSurvivorship.py
@@ -1,9 +1,18 @@
 #Joshua Mendiola - Titanic Survivorship Project
 
+#initial imports for the project, taking in pandas for math calculations and matplot lib for modeling
 import numpy as np
 import pandas as pd
 import seaborn as sbn
 import matplotlib.pyplot as plt
+#grabs the training and test dataset using the pandas readcsv function
+training = pd.read_csv('/Users/joshuamendiola/Desktop/Data Science Practice/Datasets/train.csv')
+testing = pd.read_csv('/Users/joshuamendiola/Desktop/Data Science Practice/Datasets/test.csv')
 
-training = pd.read_csv('/kaggle/input/titanic/train.csv')
+training['train_test'] = 1
+testing['train_test'] = 0
+testing['survived'] = np.NaN
+allData = pd.concat([training,testing])
+
+print(allData.columns)
 
